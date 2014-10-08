47132a6d4c7cf0c299f0698e9a39c892d7b9dd28
Jeremy Lusk <Jeremy.Lusk@Gmail.com>
2014-10-08 15:48:52 -0500

    Changed the names of constants in the Planck function
    Using C1 and C2 rather than a and b makes it (I hope) more clear that
    these are not, themselves, physical constants, but rather placeholders
    to make the declaration of the Planck function more clear.

--- bh_integration/planck.py
+++ bh_integration/planck.py
@@ -3,9 +3,9 @@ import math
 
 def planck_function(wavelength, temperature):
 
-    a = 2.0 * constants.h * constants.c**2
-    b = constants.h * constants.c / constants.k
+    C1 = 2.0 * constants.h * constants.c**2
+    C2 = constants.h * constants.c / constants.k
 
-    B_lambda = (a / wavelength**5)/(math.exp(b / (wavelength * temperature)) - 1)
+    B_lambda = (C1 / wavelength**5)/(math.exp(C2 / (wavelength * temperature)) - 1)
     
     return B_lambda
